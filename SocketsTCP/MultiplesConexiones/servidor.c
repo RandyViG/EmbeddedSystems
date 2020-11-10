e4972533f426970afdd6b3c7352e68b3a66b85e3
RandyVig <rvillanuevag1603@alumno.ipn.mx>
2020-11-10 13:42:38 -0600

    Otganizando código

--- SocketsTCP/MultiplesConexiones/servidor.c
+++ SocketsTCP/MultiplesConexiones/servidor.c
@@ -14,45 +14,19 @@
 #define EVER 1
 
 int sockfd;
+struct sockaddr_in direccion_servidor;
 
 void manjeador( int sig );
+void atiende_cliente( int cliente_sockfd );
+void ini_servidor( void );
+void ini_signals( void );
 
 int main(int argc, char **argv){
 	int cliente_sockfd;
-	struct sockaddr_in direccion_servidor;
-	char leer_mensaje[TAM_BUFFER];
 	pid_t pid;
 	
-	memset( &direccion_servidor, 0, sizeof(direccion_servidor) );
-	direccion_servidor.sin_family = AF_INET;
-	direccion_servidor.sin_port = htons(PUERTO);
-	direccion_servidor.sin_addr.s_addr = INADDR_ANY;
-
-	if( signal( SIGCHLD, manjeador ) == SIG_ERR ){
-		perror("Error en el manejador");
-		exit( EXIT_FAILURE );
-	}
-	if( signal( SIGINT, manjeador ) == SIG_ERR ){
-		perror("Error en el manejador");
-		exit( EXIT_FAILURE );
-	}
-	printf("Creando Socket ....\n");
-	if( ( sockfd = socket(AF_INET, SOCK_STREAM, 0) ) < 0 ){
-		perror("Ocurrio un problema en la creacion del socket\n");
-		exit( EXIT_FAILURE );
-	}
-
-	printf("Configurando socket ...\n");
-	if( bind( sockfd, (struct sockaddr *)&direccion_servidor, sizeof(direccion_servidor) ) < 0 ){
-		perror ("Ocurrio un problema al configurar el socket\n");
-		exit(EXIT_FAILURE );
-	}
-	
-	printf("Estableciendo la aceptacion de clientes...\n");
-	if( listen(sockfd, COLA_CLIENTES) < 0 ){
-		perror("Ocurrio un problema al crear la cola de aceptar peticiones de los clientes\n");
-		exit( EXIT_FAILURE );
-	}
+	ini_signals();
+	ini_servidor();
 
 	for( ; EVER ;  ){
 		printf("En espera de peticiones de conexión ...\n");
@@ -61,23 +35,63 @@ int main(int argc, char **argv){
 			exit( EXIT_FAILURE );
 		}
 		pid = fork();
-		if( !pid ){
-			printf("Se aceptó un cliente, atendiendolo \n");
-			if( read(cliente_sockfd, leer_mensaje, TAM_BUFFER) < 0 ){
-				perror ("Ocurrio algun problema al recibir datos del cliente");
-				exit( EXIT_FAILURE );
-			}
-			printf("El cliente nos envio el siguiente mensaje: \n %s \n", leer_mensaje);
-			if( write(cliente_sockfd, "Bienvenido cliente", 19) < 0 ){
-				perror("Ocurrio un problema en el envio de un mensaje al cliente");
-				exit( EXIT_FAILURE );
-			}	
-			close( cliente_sockfd );
-			exit( 0 );
-		}
+		if( !pid )
+			atiende_cliente( cliente_sockfd );
 	}
 
 	return 0;
+}
+
+void ini_signals( void ){
+	if( signal( SIGCHLD, manjeador ) == SIG_ERR ){
+		perror("Error en el manejador");
+		exit( EXIT_FAILURE );
+	}
+	if( signal( SIGINT, manjeador ) == SIG_ERR ){
+		perror("Error en el manejador");
+		exit( EXIT_FAILURE );
+	}
+}
+
+void ini_servidor( void ){
+	memset( &direccion_servidor, 0, sizeof(direccion_servidor) );
+	direccion_servidor.sin_family = AF_INET;
+	direccion_servidor.sin_port = htons(PUERTO);
+	direccion_servidor.sin_addr.s_addr = INADDR_ANY;
+
+	printf("Creando Socket ....\n");
+	if( ( sockfd = socket(AF_INET, SOCK_STREAM, 0) ) < 0 ){
+		perror("Ocurrio un problema en la creacion del socket\n");
+		exit( EXIT_FAILURE );
+	}
+	printf("Configurando socket ...\n");
+	if( bind( sockfd, (struct sockaddr *)&direccion_servidor, sizeof(direccion_servidor) ) < 0 ){
+		perror ("Ocurrio un problema al configurar el socket\n");
+		exit(EXIT_FAILURE );
+	}
+	printf("Estableciendo la aceptacion de clientes...\n");
+	if( listen(sockfd, COLA_CLIENTES) < 0 ){
+		perror("Ocurrio un problema al crear la cola de aceptar peticiones de los clientes\n");
+		exit( EXIT_FAILURE );
+	}
+}
+
+void atiende_cliente( int cliente_sockfd ){
+	char leer_mensaje[TAM_BUFFER];
+
+	printf("Se aceptó un cliente, atendiendolo \n");
+	if( read(cliente_sockfd, leer_mensaje, TAM_BUFFER) < 0 ){
+		perror ("Ocurrio algun problema al recibir datos del cliente");
+		exit( EXIT_FAILURE );
+	}
+	printf("El cliente nos envio el siguiente mensaje: \n %s \n", leer_mensaje);
+	if( write(cliente_sockfd, "Bienvenido cliente", 19) < 0 ){
+		perror("Ocurrio un problema en el envio de un mensaje al cliente");
+		exit( EXIT_FAILURE );
+	}	
+	close( cliente_sockfd );
+
+	exit( 0 );
 }
 
 void manjeador( int sig ){
